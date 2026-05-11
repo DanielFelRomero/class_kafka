567f03f29e2500e36b19419bf5e99db6b5a20552
Daniel Felipe Romero <69281379+DanielFelRomero@users.noreply.github.com>
2026-05-10 23:16:56 -0500

    Update churn_ml_kafka.py

--- dags/churn_ml_kafka.py
+++ dags/churn_ml_kafka.py
@@ -6,6 +6,7 @@ import numpy as np
 import pickle
 import json
 import base64
+import time
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
