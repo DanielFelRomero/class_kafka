826a23d6a8cc83c0f1f8749eae18ba3c2f2f8f80
Daniel Felipe Romero <69281379+DanielFelRomero@users.noreply.github.com>
2026-05-10 22:10:53 -0500

    Update consumer_lr.py

--- consumers/consumer_lr.py
+++ consumers/consumer_lr.py
@@ -15,35 +15,39 @@ c.subscribe(['topic_model_lr', 'topic_test_batch'])
 model = None
 print("Escuchando modelo LR y datos de test...")
 
-while True:
-    msg = c.poll(timeout = 1.0)
-    if msg is None: continue
-    if msg.error(): continue
+try:
+    while True:
+        msg = c.poll(timeout = 1.0)
+        if msg is None: continue
+        if msg.error(): continue
 
-    if msg.topic() == 'topic_model_lr':
-        model = pickle.loads(msg.value())
-        print("Modelo LR recibido vía Pickle.")
-        
-    elif msg.topic() == 'topic_test_batch' and model is not None:
-        test_data = json.loads(msg.value().decode('utf-8'))
-        df = pd.DataFrame(test_data)
-        
-        X_test = df[['usage_minutes', 'customer_service_calls', 'monthly_charge']]
-        y_test = df['churn_risk']
-        predictions = model.predict(X_test)
-        
-        print("\n--- MÉTRICAS TEST LINEAR REGRESSION ---")
-        print(f"R2: {r2_score(y_test, predictions):.4f}")
-        print(f"MAE: {mean_absolute_error(y_test, predictions):.4f}")
-        print(f"RMSE: {mean_squared_error(y_test, predictions, squared=False):.4f}")
-        
-        # Guardar para Streamlit
-        df['prediction_lr'] = predictions
-        df.to_csv('/opt/airflow/data/ml/results_lr.csv', index=False)
+        if msg.topic() == 'topic_model_lr':
+            model = pickle.loads(msg.value())
+            print("Modelo LR recibido vía Pickle.")
+            
+        elif msg.topic() == 'topic_test_batch' and model is not None:
+            test_data = json.loads(msg.value().decode('utf-8'))
+            df = pd.DataFrame(test_data)
+            
+            X_test = df[['usage_minutes', 'customer_service_calls', 'monthly_charge']]
+            y_test = df['churn_risk']
+            predictions = model.predict(X_test)
+            
+            print("\n--- MÉTRICAS TEST LINEAR REGRESSION ---")
+            print(f"R2: {r2_score(y_test, predictions):.4f}")
+            print(f"MAE: {mean_absolute_error(y_test, predictions):.4f}")
+            print(f"RMSE: {mean_squared_error(y_test, predictions, squared=False):.4f}")
+            
+            # Guardar para Streamlit
+            df['prediction_lr'] = predictions
+            df.to_csv('/opt/airflow/data/ml/results_lr.csv', index=False)
 
-        print("\nProcesamiento terminado.")
+            print("\nProcesamiento terminado.")
 
-        break
+            break
+
+except KeyboardInterrupt:
+    print("\nInterrumpido por el usuario.")
 finally:
     print("Cerrando consumer...")
     c.close()
